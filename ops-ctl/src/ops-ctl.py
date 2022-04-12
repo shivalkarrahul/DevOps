804c5be8832a879483c9b79afe0bf75b14bc7b7a
shivalkarrahul <rahulshivalkar@rediffmail.com>
2022-04-12 18:10:27 +0530

    ops-ctl tool

---
+++ ops-ctl/src/ops-ctl.py
@@ -0,0 +1,30 @@
+import argparse
+
+global args
+
+def opsCtl():
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-o', '--operation', required=True, action="store", dest="operation",
+                    help="Operation that you want to perform using ops-ctl.", default=None, choices=['generate', 'create', 'delete'], type=str)
+    parser.add_argument('-t', '--template', required=True, action="store", dest="tool",
+                    help="Template that you want to generate using ops-ctl", default=None, choices=['jenkinsfile', 'dockerfile', 'helmchart'], type=str)
+    parser.add_argument('-m', '--microservice', required=True, action="store", dest="microservice",
+                    help="Name of the microservice that you want to specify in the templates using ops-ctl", default=None, type=str)
+    args = parser.parse_args()
+
+
+    operation = args.operation
+    tool = args.tool
+    microservice = args.microservice
+
+    print("Printing values")
+    print("operation", operation)
+    print("tool", tool)
+    print("microservice", microservice)
+
+
+if __name__ == '__main__':
+    opsCtl() # pragma: no cover    
