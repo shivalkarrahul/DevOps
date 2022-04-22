4ba060486ec8dc659cd605524338c2d4ec5320e7
shivalkarrahul <rahulshivalkar@rediffmail.com>
2022-04-22 17:14:13 +0530

    added command and sub-commands

--- opsctl/opsctl.py
+++ opsctl/opsctl.py
@@ -12,49 +12,46 @@ global args
 
 def opsCtl():
 
+    # parser = argparse.ArgumentParser(prog='opsctl.py')
 
-    parser = argparse.ArgumentParser(prog='opsctl.py')
+    # parser.add_argument('-o', '--operation', required=True, action="store", dest="operation",
+    #                 help="Operation that you want to perform using opsctl.", default=None, choices=['generate'], type=str)
+    # parser.add_argument('-t', '--template', required=True, action="store", dest="tool",
+    #                 help="Template that you want to generate using opsctl", default=None, choices=['jenkinsfile', 'dockerfile', 'helmchart', 'microservice'], type=str)
+    # parser.add_argument('-m', '--microservice', required=True, action="store", dest="microservice",
+    #                 help="Type of the microservice for which you want to generate the templates using opsctl", default=None, choices=['java', 'nodejs', 'python'], type=str)
+    # parser.add_argument('-n', '--name', required=False, action="store", dest="name",
+    #                 help="Name of the microservice that you want to specify in the templates using opsctl", default="my-sample-service", type=str)
 
-    parser.add_argument('-o', '--operation', required=True, action="store", dest="operation",
-                    help="Operation that you want to perform using opsctl.", default=None, choices=['generate'], type=str)
-    parser.add_argument('-t', '--template', required=True, action="store", dest="tool",
-                    help="Template that you want to generate using opsctl", default=None, choices=['jenkinsfile', 'dockerfile', 'helmchart', 'microservice'], type=str)
-    parser.add_argument('-m', '--microservice', required=True, action="store", dest="microservice",
-                    help="Type of the microservice for which you want to generate the templates using opsctl", default=None, choices=['java', 'nodejs', 'python'], type=str)
-    parser.add_argument('-n', '--name', required=False, action="store", dest="name",
-                    help="Name of the microservice that you want to specify in the templates using opsctl", default="my-sample-service", type=str)
+    # # subparsers = parser.add_subparsers(help='sub-command help')
 
+    # args = parser.parse_args()
 
-    # subparsers = parser.add_subparsers(help='sub-command help')
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='command')
+
+    generate = subparser.add_parser('generate')
+    deploy = subparser.add_parser('deploy')
+
+    generate.add_argument('-t', '--template', type=str, required=True, choices=['jenkinsfile', 'dockerfile', 'helmchart', 'microservice'], help="Tool Template that you want to generate using opsctl")
+    generate.add_argument('-m', '--microservice', type=str, required=True, choices=['java', 'nodejs', 'python'], help="Type of Microservice that you want to generate using opsctl")
+    generate.add_argument('-n', '--name', type=str, required=True, help="Name of Microservice that you want to generate using opsctl", default="my-sample-repo")
+
+    deploy.add_argument('-a', '--application', type=str, required=True, choices=['jenkins', 'microservice'], help="Application that you want to deploy using opsctl")
 
     args = parser.parse_args()
 
+    if args.command == 'generate':
+        print('Generating a template:', args.template , 'for microservice of type:', args.microservice, 'with name:', args.name)
+    elif args.command == 'deploy':
+        print('Deploying an application:', args.application)
 
-
-
-
-    operation = args.operation
-    tool = args.tool
-    microservice = args.microservice
-    name = args.name
-
-    print("Printing values")
-    print("operation", operation)
-    print("template", tool)
-    print("microservice", microservice)
-    print("name", name)
-
-    generate_dockerfile.generate(microservice, name)
-    generate_jenkinsfile.generate(microservice, name)
-    generate_helmchart.generate()
-    # generate_java_microservice.generate(microservice, name)
-    # generate_nodejs_microservice.generate(microservice, name)
-    generate_microservice.generate(microservice, name)
-
-
-
-
-
+    # generate_dockerfile.generate(microservice, name)
+    # generate_jenkinsfile.generate(microservice, name)
+    # generate_helmchart.generate()
+    # # generate_java_microservice.generate(microservice, name)
+    # # generate_nodejs_microservice.generate(microservice, name)
+    # generate_microservice.generate(microservice, name)
 
 
 if __name__ == '__main__':
